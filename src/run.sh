635b86571391fea08298ab61c4f0b9c2442f44b9
oklookat <85795332+oklookat@users.noreply.github.com>
2021-07-15 03:43:21 +0300

    oh, docker

--- src/run.sh
+++ src/run.sh
@@ -1,11 +1,4 @@
 BACKEND_DIR="backend"
 ADMIN_FRONTEND_DIR="elvenfront"
 
-cd $BACKEND_DIR
-npm install
-cd ..
-cd $ADMIN_FRONTEND_DIR
-npm install
-cd ..
-docker-compose build
-docker-compose up
+docker-compose up --build
